8cd8e15aa3b5ab2d697e3331859cc736bf45ff71
Bittu kumar prajapati <109884531+Bittu-kumar2003@users.noreply.github.com>
2024-04-07 11:56:46 +0530

    Update Program_2619.js

--- JavaScript/Array Protypes Last/Program_2619.js
+++ JavaScript/Array Protypes Last/Program_2619.js
@@ -1,3 +1,5 @@
+//Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
